aa77a03ff4116fc8471a5d40ed4ec81378235e4b
Kalpanatt86 <kalpanatt86@gmail.com>
2020-12-18 15:39:30 -0500

    pushing the code from eclipse to github

---
+++ gitproject/src/main/java/pages/LoginPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class LoginPage {
+	public void loginPage() {
+		System.out.println("This is a login page");
+	}
+}
---
+++ gitproject/src/test/java/tests/LoginTests.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class LoginTests {
+	public void loginPage() {
+		System.out.println("This is a login test case");
+	}
+}
