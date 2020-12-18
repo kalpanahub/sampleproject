77b07f415693362217fabaafece47c0ae8a9d278
Kalpana2020 <Kalpana2020@192.168.200.247>
2020-12-18 16:25:32 -0500

    Added 2nd print line in both classes

--- gitproject/src/main/java/pages/LoginPage.java
+++ gitproject/src/main/java/pages/LoginPage.java
@@ -3,5 +3,6 @@ package pages;
 public class LoginPage {
 	public void loginPage() {
 		System.out.println("This is a login page");
+		System.out.println("Doing changes 2nd time to push in github");
 	}
 }
--- gitproject/src/test/java/tests/LoginTests.java
+++ gitproject/src/test/java/tests/LoginTests.java
@@ -3,5 +3,6 @@ package tests;
 public class LoginTests {
 	public void loginPage() {
 		System.out.println("This is a login test case");
+		System.out.println("Doing changes for login tests for the 2nd time");
 	}
 }
