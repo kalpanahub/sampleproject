02a7f6f86378ef411569ce73fc8c0e534d9a6fba
Kalpana2020 <Kalpana2020@192.168.200.247>
2020-12-18 23:17:17 -0500

    Added Sysout line

--- gitproject/src/main/java/pages/LoginPage.java
+++ gitproject/src/main/java/pages/LoginPage.java
@@ -4,5 +4,7 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("This is a login page");
 		System.out.println("Doing changes 2nd time to push in github");
+		System.out.println("Doing one more time");
+		System.out.println("Doing again");
 	}
 }
