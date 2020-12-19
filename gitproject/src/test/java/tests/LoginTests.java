36b7ead2a719733359d5a6ab8236122082f698ef
Kalpanahub <kalpanatt86@gmail.com>
2020-12-18 23:01:17 -0500

    Added sysout line

--- gitproject/src/test/java/tests/LoginTests.java
+++ gitproject/src/test/java/tests/LoginTests.java
@@ -4,5 +4,7 @@ public class LoginTests {
 	public void loginPage() {
 		System.out.println("This is a login test case");
 		System.out.println("Doing changes for login tests for the 2nd time");
+		System.out.println("Doing third time changes");
+		
 	}
 }
