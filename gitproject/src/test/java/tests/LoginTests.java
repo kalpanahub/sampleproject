0eaa1ed7a16c55d8886ac58bca7ed606ffef3e72
Kalpana2020 <Kalpana2020@192.168.200.248>
2020-12-22 18:28:26 -0500

    Created HomePageTests.java Added Sysout line in LoginTests

--- gitproject/src/test/java/tests/LoginTests.java
+++ gitproject/src/test/java/tests/LoginTests.java
@@ -5,6 +5,7 @@ public class LoginTests {
 		System.out.println("This is a login test case");
 		System.out.println("Doing changes for login tests for the 2nd time");
 		System.out.println("Doing third time changes");
+		System.out.println("Doing 4th time changes");
 		
 	}
 }
