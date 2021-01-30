c2c0694d9b49e2d422b18f76ecdee2244872f638
kalpanahub <kalpanatt86@gmail.com>
2021-01-30 17:27:59 -0500

    Update HomePageTests.java

--- gitproject/src/test/java/tests/HomePageTests.java
+++ gitproject/src/test/java/tests/HomePageTests.java
@@ -3,5 +3,6 @@ package tests;
 public class HomePageTests {
 	public void homePageTests() {
 		System.out.println("Test case is passed");
+		System.out.println("Doing it from github for employee2");
 	}
 }
